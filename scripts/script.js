11a67a3023353c308e5bcaa72818841c88b90396
cschmi14 <cschmidt542@gmail.com>
2021-09-28 20:39:58 -0400

    updated courses table

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const header = document.querySelector('.header');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -9,4 +10,17 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
+});
+
+const courseToggle = document.querySelector('.education-one__button');
+const returnButton = document.querySelector('.courses-return__button');
+
+courseToggle.addEventListener('click', () => {
+    document.body.classList.toggle('course-open');
+    header.style.display = "none";
+});
+
+returnButton.addEventListener('click', () => {
+    document.body.classList.remove('course-open');
+    header.style.display = "flex";
 });
